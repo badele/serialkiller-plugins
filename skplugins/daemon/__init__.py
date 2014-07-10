798686a915bb89a50d38273e59400f6f63b0c8c1
badele <bruno.adele@jesuislibre.org>
2014-07-10 13:35:03 +0200

    Add X10 plugin

---
+++ skplugins/daemon/__init__.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__authors__ = 'Bruno Adelé <bruno@adele.im>'
+__copyright__ = 'Copyright (C) 2013 Bruno Adelé'
+__description__ = """A plugins for serialkiller project"""
+__license__ = 'GPL'
+__version__ = '0.0.2'
+__apiversion__ = '1.0'
+
+import os
+import sys
+import json
+import requests
+import time
+import threading
+
+from collections import defaultdict
+
+
+class daemon(threading.Thread):
+    """Generic Class for type"""
+    def __init__(self, **kwargs):
+        threading.Thread.__init__(self)
+        self._stopevent = threading.Event( )
+
+        # Set parameters
+        self._params = kwargs
+        self._params['threadwait'] = 2
+
+        self._results = defaultdict(lambda: None)
+        self._types = dict()
+
+    @property
+    def params(self):
+        """Get Value"""
+        return self._params
+
+    @params.setter
+    def params(self, value):
+        self._params = value
+
+    @property
+    def result(self):
+        """Get Value"""
+        if 'result' not in self.results:
+            return None
+        else:
+            return self.results['result']
+
+    @property
+    def results(self):
+        """Get Value"""
+        return self._results
+
+    @results.setter
+    def results(self, value):
+        """Set Value"""
+        self._results = value
+
+    @property
+    def type(self):
+        if 'result' not in self._types:
+            raise Exception("type not found for result")
+
+        return self._types['result']
+
+    @property
+    def types(self):
+        return self._types
+
+    def stop(self):
+        self._stopevent.set( )
+
+    def run(self):
+        """Check Sensor"""
+        # noinspection PyProtectedMember
+        mess = "%s.%s" % (self.__class__, sys._getframe().f_code.co_name)
+        raise NotImplementedError(mess)
+
---
+++ skplugins/daemon/protocol/x10.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__authors__ = 'Bruno Adelé <bruno@adele.im>'
+__copyright__ = 'Copyright (C) 2013 Bruno Adelé'
+__description__ = """A plugins for serialkiller project"""
+__license__ = 'GPL'
+__version__ = '0.0.2'
+
+# On the server require mochad application
+
+import re
+import os
+import socket
+from collections import defaultdict
+
+from skplugins.daemon import daemon
+
+class x10(daemon):
+    """Check the x10 status"""
+    def __init__(self, **kwargs):
+        super(x10, self).__init__(**kwargs)
+        self._types = {
+            'result': None,
+        }
+        self.initPlugin()
+
+    def initPlugin(self):
+            self.results['result'] = defaultdict(lambda: None)
+
+    def executeMochadCommand(self, command):
+            if 'mochad' not in self.params:
+                self.results['result'] = None
+                return
+
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.params['mochad'], 1099))
+            s.sendall("%s\n" % command)
+            s.shutdown(socket.SHUT_WR)
+
+            out = ''
+            while 1:
+                data = s.recv(1024)
+                if data == '':
+                    break
+
+                out += data
+
+            s.close()
+            return out
+
+    def checkStatus(self):
+
+            out = self.executeMochadCommand('st')
+            results = re.findall(r'House ([A-O]): ([0-9A-F]=.*)', out)
+            if results:
+                for r in results:
+                    x10house = r[0]
+                    x10sensors = r[1].split(',')
+
+                    for x10sensor in x10sensors:
+                        (sensorid, value) = x10sensor.split('=')
+                        self.results['result']["%s%s" % (x10house, sensorid)] = int(value)
+
+    def power(self,unit, value):
+            self.executeMochadCommand('pl %s %s' % (unit, value))
+
+    def run(self):
+
+        # Loop mochad content
+        while not self._stopevent.isSet():
+
+            self.checkStatus()
+
+            self._stopevent.wait(self.params['threadwait'])
